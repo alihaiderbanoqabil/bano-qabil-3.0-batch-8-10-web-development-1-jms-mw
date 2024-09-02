ec91a3b22146e887f2137a9591f3ee029945b627
shaheer0341 <muhammadshaheeredu@gmail.com>
2024-09-02 22:06:05 +0500

    done with class 17 and 18 code

---
+++ class17/script.js
@@ -0,0 +1,7 @@
+console.log(`Hello this is our first class of JS.`);
+var fullName = "Ali haider";
+var fullName = "hasnain";
+var fullName = 33;
+// var n1 = 30;
+
+console.log("Hello my name is", fullName);
---
+++ class18/script.js
@@ -0,0 +1,84 @@
+console.log("Class 2 of JavaScript");
+// // here i store my name in xyz variable
+// // console.log(xyz);
+// // var var1, var2, var3;
+
+// // var1 = 45;
+// // var2 = "strnb";
+// // var3 = true;
+
+// // var var1 = 45,
+// //   var2 = "strnb",
+// //   var3 = true;
+
+// console.log(name, "before");
+// var xyz = "Ali haider";
+// var fullName = "hasnain";
+// var fullName = 33;
+// var isAdult = true;
+// // var n1 = 30;
+// // const student1 = {
+// //   name: "ali",
+// //   gender: "male",
+// //   age: 26,
+// //   fullName: function () {
+// //     return "Ali Haider";
+// //   },
+// // };
+// // const student2 = {
+// //   name: "ali",
+// //   gender: "male",
+// //   age: 26,
+// //   fullName: function () {
+// //     return "Ali Haider";
+// //   },
+// // };
+// // console.log(student1);
+
+// function log(message) {
+//   let abba = "jhkjs";
+//   console.log(abba);
+
+//   //   console.log(message, "message");
+// }
+// // console.log(abba);
+
+// // IIFE
+
+// // (function () {
+// //   var iife = "IIFE";
+// //   console.log(iife);
+// // })();
+// console.log(name, "after");
+// // console.log("Hello my name is", xyz);
+// console.log(window);
+
+// log("hello world");
+
+// let unique1 = "hello hello";
+// // let unique1 = "hello hello";
+// console.log(unique1);
+// unique1 = "hello";
+// const unique2 = "hello world";
+// // const unique2 = "hello world";
+// unique2 = "hello world";
+// console.log(unique1);
+// console.log(unique2);
+// console.log(name1);
+
+// const name1 = "akakjhakja";
+// console.log(name1);
+
+let string1 = `stringkjfslkjfsdlkjkljfd
+
+
+lsdjlkjkfldjkldglkjd
+
+
+kjlkfdjlkfdjlkgjlkgfdjlkgjflkfd
+klffdhfdkj
+
+`;
+let string2 = "string";
+console.log(string2);
+console.log(string1);
