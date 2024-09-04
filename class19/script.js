7bcf990fe3e6fb604217e899b462be8a985a659d
shaheer0341 <muhammadshaheeredu@gmail.com>
2024-09-04 22:12:57 +0500

    done with class19 and class3 of js

---
+++ class19/script.js
@@ -0,0 +1,197 @@
+console.log("Class 3 od JS");
+
+let string1 = `stringkjfslkjfsdlkjkljfd
+
+
+lsdjlkjkfldjkldglkjd
+
+
+kjlkfdjlkfdjlkgjlkgfdjlkgjflkfd
+klffdhfdkj
+
+`;
+
+let name = "ali haider";
+
+let age = 30;
+
+// console.log("My name is " + name + " and my age is " + age);
+const greet = `My name is ${name} and my age is ${age}`;
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+// console.log(greet);
+
+// loop in js
+// const std1 = "ali1";
+// const std2 = "ali2";
+// const std3 = "ali3";
+// const std4 = "ali4";
+// const std5 = "ali5";
+// const std6 = "ali6";
+// const std7 = "ali7";
+
+const students = ["ali1", "ali2", "ali3", "ali4", "ali5", "ali6", "ali7"];
+console.log(students, "students");
+// console.log(students[0], "students");
+// console.log(students[1], "students");
+// console.log(students[2], "students");
+console.log(students.length, "students array count");
+
+// let index = 0;
+// console.log(index);
+// index = index + 3;
+// console.log(index);
+// index = index + 1;
+// index++;
+// index++;
+// index++;
+// index++;
+// index++;
+// console.log(index);
+
+// console.log(element, "element");
+
+// for (let index = 0; index < students.length; index++) {
+//   const element = students[index];
+//   console.log(element, "element", index);
+//   //   console.log(greet);
+// }
+
+// for (let index = 0; index < 5; index++) {
+//   console.log(greet, index);
+// }
+
+// for (let index = 0; index < students.length; index--) {
+//   const element = students[index];
+//   console.log(element, "element", index);
+// }
+
+// for (let index = students.length - 1; index >= 0; index--) {
+//   const element = students[index];
+//   console.log(element, "element", index);
+// }
+
+// for (let i = 0; i < 5; i++) {
+//   console.log("Outer loop", i);
+
+//   for (let j = 0; j < 5; j++) {
+//     console.log(`Inner loop i=${i} j=${j}`);
+//   }
+// }
+
+// const enteredNumber = prompt("Guess a number from 1 to 10: ");
+// let number = parseInt(enteredNumber);
+// console.log(
+//   typeof number,
+//   "number",
+//   number,
+//   typeof enteredNumber,
+//   enteredNumber
+// );
+
+// let index = 10;
+// while (index < students.length) {
+//   const element = students[index];
+//   console.log(element, "element", index);
+//   index++;
+// }
+
+// let i = 10;
+// do {
+//   console.log("i: ", i);
+
+//   i++;
+// } while (i < 5);
+
+// Operators
+// console.log(Math.pow(10, 2));
+// console.log(10 ** 2);
+
+// let x = 17;
+// let y = 3;
+// let z = x % y;
+
+// console.log(17 % 3);
+
+// let n1 = 17;
+// let n2 = 3;
+// let sum1 = n1 + n2;
+// n1 = n1 + n2;
+// n1 += n2;
+// let min = n1 - n2;
+// let mul = n1 * n2;
+// console.log(min);
+// console.log(mul);
+// console.log(sum);
+
+// 120 == 120
+// true
+// 'ali' == 'ali'
+// true
+// 'ali' == 'aali'
+// false
+// 120 == 121
+// false
+// 'ali' === 'ali'
+// true
+// 'ali' === 'aali'
+// false
+// 10 == '10'
+// true
+// 10 === '10'
+// false
+// 10 != 20
+// true
+// 'ali' != 'hasnian'
+// true
+// 'ali' != 'ali'
+// false
+// 10 != 10
+// false
+// 10 != '10'
+// false
+// 10 != '22'
+// true
+// 10 !== '22'
+// true
+// 10 !== '10'
+// true
