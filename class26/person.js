37d358805d37ecda2c1723afb7242524d61a7cfa
alihaiderbanoqabil <alihaider.banoqabil2.0@gmail.com>
2024-10-02 21:08:11 +0500

    done with Class 26 (Class 10 of JS)

---
+++ class26/person.js
@@ -0,0 +1,72 @@
+// export const name = "Jesse";
+// export const age = 40;
+// export const user = {
+//     "id": 1,
+//     "name": "Leanne Graham",
+//     "username": "Bret",
+//     "email": "Sincere@april.biz",
+//     "address": {
+//         "street": "Kulas Light",
+//         "suite": "Apt. 556",
+//         "city": "Gwenborough",
+//         "zipcode": "92998-3874",
+//         "geo": {
+//             "lat": "-37.3159",
+//             "lng": "81.1496"
+//         }
+//     },
+//     "phone": "1-770-736-8031 x56442",
+//     "website": "hildegard.org",
+//     "company": {
+//         "name": "Romaguera-Crona",
+//         "catchPhrase": "Multi-layered client-server neural-net",
+//         "bs": "harness real-time e-markets"
+//     }
+// }
+const name = "Jesse";
+const age = 40;
+const user = {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+            "lat": "-37.3159",
+            "lng": "81.1496"
+        }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+        "name": "Romaguera-Crona",
+        "catchPhrase": "Multi-layered client-server neural-net",
+        "bs": "harness real-time e-markets"
+    }
+}
+
+export {
+    name, age, user
+}
+
+
+const message = (parameter) => {
+    const name = "Jesse";
+    const age = 40;
+    return name + ' is ' + age + ' years old.' + parameter;
+};
+
+export default message;
+
+
+// const message1 = () => {
+//     const name = "Jesse";
+//     const age = 40;
+//     return name + ' is ' + age + 'years old.';
+// };
+
+// export default message1;
